b2f2e362dc1b7a96c22919fc19a09b5ff4a3495b
Sambhav Kumar Thakur <sambhavkumar228@gmail.com>
2021-03-05 21:36:22 +0530

    Add files via upload

---
+++ src/main/java/com/epam/assignment/Math/Mathsjava.java
@@ -0,0 +1,51 @@
+package com.epam.assignment.Math;
+
+public class Mathsjava
+{
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+	public static float add(float a, float b) {
+		return a + b;
+	}
+	public static double add(double a, double b) {
+		return a + b;
+	}
+	
+	// Subtraction
+	
+	public static int subtract(int a, int b) {
+		return a - b;
+	}
+	public static float subtract(float a, float b) {
+		return a - b;
+	}
+	public static double subtract(double a, double b) {
+		return a - b;
+	}
+	
+	// Multiplication
+	
+	public static int multiply(int a, int b) {
+		return a * b;
+	}
+	public static float multiply(float a, float b) {
+		return a * b;
+	}
+	public static double multiply(double a, double b) {
+		return a * b;
+	}
+	
+	// Division
+	
+	public static int divison(int a, int b) throws ArithmeticException {
+		return a / b;
+	}
+	public static float divison(float a, float b) throws ArithmeticException {
+		return a / b;
+	}
+	public static double divison(double a, double b) throws ArithmeticException {
+		return a / b;
+	}
+}
